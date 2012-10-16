59b4dfd0a298677c9116e5b2b51c20734273f258
Tanoy Sinha <tsinha@gmail.com>
2012-10-16 11:03:34 -0400

    Initial Commit

---
+++ M3Modeler/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  M3Modeler
+//
+//  Created by Tanoy Sinha on 10/16/12.
+//  Copyright (c) 2012 Tanoy Sinha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
